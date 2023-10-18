ee0adee4bca210863631bc4d95838dd90d1866ed
Saptarino <spta@localhost.localdomain>
2023-10-19 04:49:59 +0800

    base project

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            'name' => ['string', 'max:70'],
+            'email' => ['string', 'email', 'max:70', 'unique:users'],
+            'username' => ['required', 'string', 'max:15', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'username' => $input['username'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
---
+++ app/Enums/KelasType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum KelasType
+{
+    const Pagi = 'pagi';
+    const Sore = 'sore';
+    const Eksekutif = 'eksekutif';
+}
